5e37ee1c2612d6a222cc4ca80a39c0b9d1dd58e1
Alexandra Kanik <alexandra@rationalact.com>
2017-03-06 19:26:33 -0500

    update this branch with what is live remotely

--- scripts/fetch-img.py
+++ scripts/fetch-img.py
@@ -1,13 +1,20 @@
+#!/usr/bin/python3
+
 import glob, os, json
 
-os.chdir('../uploads')
+os.chdir('/var//www/html/vday/uploads/')
 
-with open('../data/all-images.json', 'w') as outfile:
+with open('/var//www/html/vday/data/all-images.json', 'w') as outfile:
     
     images_json = []
     
     for file in glob.glob('*.png'):
-        images_json.append(file)
+        single_image = {}
+        single_image['imageID'] = file.rpartition('.')[0]
+        single_image['imageURL'] = file
+        
+        images_json.append(single_image)
     
+    #print(images_json)
     json.dump(images_json, outfile)
     
